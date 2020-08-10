38f9d8ba6aa761c36f6239c2fd740285447fad55
AVVS <v@makeomatic.ca>
2020-08-10 15:41:52 -0700

    chore: remove -x mode from sh

--- charts/installer/test.sh
+++ charts/installer/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 BASEDIR=$(dirname "$0")
 
--- scripts/sync-charts.sh
+++ scripts/sync-charts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+set -e
 shopt -s expand_aliases
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
