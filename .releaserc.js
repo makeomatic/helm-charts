887fee7b8d185ff9b809d28fd934d2164d29da6a
Eugene Poyarkov <eug.poyarkov@gmail.com>
2021-03-01 13:44:13 +0000

    chore: ignore helm2

--- .releaserc.js
+++ .releaserc.js
@@ -18,7 +18,6 @@ exports.plugins = [
   }],
   "@semantic-release/changelog",
   ["@semantic-release/npm", { "npmPublish": false }],
-  ["@semantic-release/exec", { "prepareCmd": "HELM_VERSION=2.16.9 ./scripts/sync-charts.sh" }],
   ["@semantic-release/exec", { "prepareCmd": "HELM_VERSION=3.2.4 ./scripts/sync-charts.sh" }],
   "@semantic-release/git",
   "@semantic-release/github"
